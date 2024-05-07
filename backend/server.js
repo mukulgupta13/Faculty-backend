ded095681ef8fe309bd69abd0a9e1b40a1ecbae1
mukulgupta13 <136986709+mukulgupta13@users.noreply.github.com>
2024-05-07 07:48:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const cors = require('cors');
-const { saveUser, getUsers, getUser, updateUser, deleteUser } = require('./controllers/user_controller');
+const { saveUser, getUsers, getUser, updateUser, deleteUser, getFile } = require('./controllers/user_controller');
 const { savePublication, getPublication, deletePublication } = require('./controllers/publication_controller');
 const { saveUserSignup, signin } = require('./controllers/userSignup_controller');
 
@@ -16,8 +17,8 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 //Connect to mongodb
 
 mongoose.connect('mongodb://localhost:27017/FacultyDB',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+   // useNewUrlParser: true,
+   // useUnifiedTopology: true,
 })
 .then(()=>console.log('Connected to MongoDB'))
 .catch((err)=>console.error('Connection error:',err));
@@ -32,6 +33,21 @@ app.post('/signin',signin);
 app.get('/getUser',getUsers);
 app.get('/getUser/:userId',getUser);
 app.get('/getPublication/:userId',getPublication);
+app.get('/getfile/:file', getFile);
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination: function(req,file,cb)
+//         {
+//             cb(null,"uploads");
+//         },
+//         filename: function(req, file,cb){
+//             cb(null,file.fieldname + "-" + Date.now() + ".jpg")
+//         }
+//     })
+// }).single("user_file");
+// app.post('/upload',upload, (req, res) => {
+//     res.send("file upload");
+// });
 
 // Start the Server
 app.listen(3001, () => {
